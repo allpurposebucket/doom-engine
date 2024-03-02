11ca5e4f185e248daee837ad245760ad65e1dffd
allpurposebucket <dinostar237@gmail.com>
2024-03-02 13:34:31 -0500

    Prog

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,178 @@
-use glium::{Surface, implement_vertex, uniform};
+#[macro_use]
+extern crate glium;
+
+use glium::{Display, Surface};
+use glutin::surface::WindowSurface;
+use support::{ApplicationContext, State};
+
+mod support;
+
+struct Application {
+    pub vertex_buffer: glium::vertex::VertexBufferAny,
+    pub program: glium::Program,
+    pub camera: support::camera::CameraState,
+}
+
+impl ApplicationContext for Application {
+    const WINDOW_TITLE:&'static str = "Glium teapot example";
+
+    fn new(display: &Display<WindowSurface>) -> Self {
+        let vertex_buffer = support::load_wavefront(&display, include_bytes!("../assets/skull.obj"));
+        let program = program!(display,
+            140 => {
+                vertex: "
+                    #version 140
+
+                    uniform mat4 persp_matrix;
+                    uniform mat4 view_matrix;
+
+                    in vec3 position;
+                    in vec3 normal;
+                    out vec3 v_position;
+                    out vec3 v_normal;
+
+                    void main() {
+                        v_position = position;
+                        v_normal = normal;
+                        gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
+                    }
+                ",
+
+                fragment: "
+                    #version 140
+
+                    in vec3 v_normal;
+                    out vec4 f_color;
+
+                    const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
+
+                    void main() {
+                        float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
+                        vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
+                        f_color = vec4(color, 1.0);
+                    }
+                ",
+            },
+
+            110 => {
+                vertex: "
+                    #version 110
+
+                    uniform mat4 persp_matrix;
+                    uniform mat4 view_matrix;
+
+                    attribute vec3 position;
+                    attribute vec3 normal;
+                    varying vec3 v_position;
+                    varying vec3 v_normal;
+
+                    void main() {
+                        v_position = position;
+                        v_normal = normal;
+                        gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
+                    }
+                ",
+
+                fragment: "
+                    #version 110
+
+                    varying vec3 v_normal;
+
+                    const vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
+
+                    void main() {
+                        float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
+                        vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
+                        gl_FragColor = vec4(color, 1.0);
+                    }
+                ",
+            },
+
+            100 => {
+                vertex: "
+                    #version 100
+
+                    uniform lowp mat4 persp_matrix;
+                    uniform lowp mat4 view_matrix;
+
+                    attribute lowp vec3 position;
+                    attribute lowp vec3 normal;
+                    varying lowp vec3 v_position;
+                    varying lowp vec3 v_normal;
+
+                    void main() {
+                        v_position = position;
+                        v_normal = normal;
+                        gl_Position = persp_matrix * view_matrix * vec4(v_position * 0.005, 1.0);
+                    }
+                ",
+
+                fragment: "
+                    #version 100
+
+                    varying lowp vec3 v_normal;
+
+                    const lowp vec3 LIGHT = vec3(-0.2, 0.8, 0.1);
+
+                    void main() {
+                        lowp float lum = max(dot(normalize(v_normal), normalize(LIGHT)), 0.0);
+                        lowp vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
+                        gl_FragColor = vec4(color, 1.0);
+                    }
+                ",
+            },
+        )
+        .unwrap();
+
+        let camera = support::camera::CameraState::new();
+
+        Self {
+            vertex_buffer,
+            program,
+            camera,
+        }
+    }
+
+    fn draw_frame(&mut self, display: &Display<WindowSurface>) {
+        let mut frame = display.draw();
+        // building the uniforms
+        let uniforms = uniform! {
+            persp_matrix: self.camera.get_perspective(),
+            view_matrix: self.camera.get_view(),
+        };
+
+        // draw parameters
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::DepthTest::IfLess,
+                write: true,
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        frame.clear_color_and_depth((0.0, 0.0, 0.0, 0.0), 1.0);
+        frame
+            .draw(
+                &self.vertex_buffer,
+                &glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
+                &self.program,
+                &uniforms,
+                &params,
+            )
+            .unwrap();
+        frame.finish().unwrap();
+    }
+
+    fn handle_window_event(&mut self, event: &winit::event::WindowEvent, _window: &winit::window::Window) {
+        self.camera.process_input(&event);
+    }
+
+    fn update(&mut self) {
+        self.camera.update();
+    }
+}
 
 fn main() {
-    let event_loop = winit::event_loop::EventLoopBuilder::new().build().expect("event loop building");
-    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
-    
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
-        color: [f32; 3],
-    }
-    implement_vertex!(Vertex, position, color);
-    
-    let shape = vec![
-        Vertex { position: [ -0.5,  -0.5  ], color: [1.0, 0.0, 0.0] },
-        Vertex { position: [  0.0,   0.5  ], color: [0.0, 1.0, 0.0] },
-        Vertex { position: [  0.5,  -0.25 ], color: [0.0, 0.0, 1.0] }
-    ];
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
-    let vertex_shader_src = r#"
-        #version 140
-
-        in vec2 position;
-        in vec3 color;
-        out vec3 vertex_color;
-
-        uniform mat4 matrix;
-
-        void main() {
-            vertex_color = color;
-            gl_Position = matrix * vec4(position, 0.0, 1.0);
-        }
-    "#;
-
-    let fragment_shader_src = r#"
-        #version 140
-        
-        in vec3 vertex_color;
-        out vec4 color;
-
-        void main() {
-            color = vec4(vertex_color, 1.0);
-        }
-    "#;
-
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-
-    let mut frame = display.draw();
-    frame.clear_color(0.0, 0.0, 1.0, 1.0);
-    frame.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
-    frame.finish().unwrap();
-
-    let mut t: f32 = 0.0;
-    let _ = event_loop.run(move |event, window_target| {
-        match event {
-            winit::event::Event::WindowEvent { event, .. } => match event {
-                winit::event::WindowEvent::CloseRequested => window_target.exit(),
-                winit::event::WindowEvent::Resized(window_size) => {
-                    display.resize(window_size.into());
-                },
-                winit::event::WindowEvent::RedrawRequested => {
-                    t += 0.02;
-
-                    let x = t.sin() * 0.5;
-
-                    let mut target = display.draw();
-                    target.clear_color(0.0, 0.0, 1.0, 1.0);
-                    let uniforms = uniform! {
-                        matrix: [
-                            [t.cos(), t.sin(), 0.0, 0.0],
-                            [-t.sin(), t.cos(), 0.0, 0.0],
-                            [0.0, 0.0, 1.0, 0.0],
-                            [ x,  0.0, 0.0, 1.0f32],
-                        ]
-                    };
-                    target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
-                    target.finish().unwrap();
-                },
-                _ => (),
-            },
-            winit::event::Event::AboutToWait => {
-                window.request_redraw();
-            },
-            _ => (),
-        };
-    });
+    State::<Application>::run_loop();
 }
